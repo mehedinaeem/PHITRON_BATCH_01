4ee6a9617b1ee104a9e905c9587bd8975022c4e1
mehedinaeem <mehedinaeem00@gmail.com>
2023-04-08 17:21:38 +0600

    c++

---
+++ week-4/module-15/1.linked_list_creation.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *Next;
+};
+void display(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->value;
+        if(n->Next != NULL)
+        cout<<"->>";
+        n=n->Next;
+    }
+    cout<<endl<<endl;
+}
+int main()
+{
+    Node *head = new Node();
+    Node *second = new Node();
+    Node *third = new Node();
+    Node *fourth = new Node();
+
+    head->value = 1;
+    second->value = 2;
+    third->value = 3;
+    fourth->value = 4;
+
+    head->Next = second;
+    second->Next = third;
+    third->Next = fourth;
+    fourth->Next = NULL;
+
+    display(head);
+}
