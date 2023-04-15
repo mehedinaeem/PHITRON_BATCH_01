715973f5e0fc62945bdeaa8c951362fbda20d668
mehedinaeem <mehedinaeem00@gmail.com>
2023-04-15 10:43:38 +0600

    ds

--- DATA-STRUCTURE/week-4/module-15/4.input_head_tail.cpp
+++ DATA-STRUCTURE/week-4/module-15/4.input_head_tail.cpp
@@ -15,8 +15,8 @@ public:
     }
 };
 
-insertAtTail(Node * &head, int val)
-{ 
+insertAtTail(Node *&head, int val)
+{
     Node *newNode = new Node(val);
 
     if (head == NULL)
@@ -32,15 +32,14 @@ insertAtTail(Node * &head, int val)
     }
     temp->Next = newNode;
 }
-insertAtHead(Node * &head, int val)
+insertAtHead(Node *&head, int val)
 {
-    //s1: new node creation
-    Node*newNode=new Node(val);
-    //s2: update of new node next
-    newNode->Next=head;
-    //s3: update of head
-    head=newNode;
-
+    // s1: new node creation
+    Node *newNode = new Node(val);
+    // s2: update of new node next
+    newNode->Next = head;
+    // s3: update of head
+    head = newNode;
 }
 void display(Node *n)
 {
@@ -60,23 +59,23 @@ int main()
 {
     Node *head = NULL;
     int n;
-   //choice 1: insertion at head
-   cout<<"choice 1: insertion at head:"<<endl;
-   //choice 2: insertion at tail
-   cout<<"choice 2: insertion at tail:"<<endl;
-   cout<<"choice 3: Exit"<<endl;
+    // choice 1: insertion at head
+    cout << "choice 1: insertion at head:" << endl;
+    // choice 2: insertion at tail
+    cout << "choice 2: insertion at tail:" << endl;
+    cout << "choice 3: Exit" << endl;
 
-    int choice=2;
-    while(choice==1 || choice==2)
+    int choice = 2;
+    while (choice == 1 || choice == 2)
     {
-        cout<<"Enter the value:";
-        cin>>n;
-        if(choice==1)
-        insertAtHead(head,n);
-        else if(choice==2)
-        insertAtTail(head,n);
-        cout<<"Next choice:";
-        cin>>choice;
+        cout << "Enter the value:";
+        cin >> n;
+        if (choice == 1)
+            insertAtHead(head, n);
+        else if (choice == 2)
+            insertAtTail(head, n);
+        cout << "Next choice:";
+        cin >> choice;
     }
 
     // insertAtTail(head, 1);
