78a1e604e2542718005f501b6db7e2e7388aad20
mehedinaeem <mehedinaeem00@gmail.com>
2023-07-25 18:36:54 +0600

    python

---
+++ 4.Python/week-02/module-04/1.function.py
@@ -0,0 +1,12 @@
+def multi(a,b):
+    result=a*b
+    print(f"the multify is: {result}")
+
+multi(5,2)
+
+def summation(a,b):
+    sum=a+b
+    return sum
+
+output=summation(2,5)
+print(f"The sum is: {output}")
