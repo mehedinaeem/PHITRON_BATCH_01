f4b32cb7d6ca4998b973da8883bfe5adb7a7b4a9
mehedinaeem <mehedinaeem00@gmail.com>
2023-09-23 08:55:14 +0600

    class

---
+++ 4.Python/week-03/module-08/1.class.py
@@ -0,0 +1,18 @@
+class phone:
+    brand='samsung'
+    colour='black'
+    price=10000
+
+#make object
+my_phone=phone()
+print(my_phone)
+print(my_phone.brand)
+print(my_phone.colour)
+print(my_phone.price)
+
+your_phone=phone()
+print(your_phone.brand)
+
+
+father_phone=phone()
+print(father_phone.colour)
