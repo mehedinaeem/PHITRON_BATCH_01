bf88fe3911beb14871d0649b8d3ea87fc71cc2f9
mehedinaeem <mehedinaeem00@gmail.com>
2023-09-23 10:05:39 +0600

    method

---
+++ 4.Python/week-03/module-08/3.calculatir.py
@@ -0,0 +1,19 @@
+class calculator:
+    def add(self):
+        print(2+3)
+    
+    def sub(self,num1,num2):
+        print(num1-num2)
+
+    def multi(self):
+        print(2*2)
+
+    def div(self):
+        print(int(2/2))
+
+my_cal=calculator()
+my_cal.add()
+my_cal.sub(100,5)
+my_cal.multi()
+my_cal.div()
+
